13363e4741672c45578db1fb9ddf01ec4af6a7e8
KwangTaeKim <ktkim@elicer.com>
2022-03-29 05:31:03 +0000

    back api에서 기본 URL(/) 삭제 -> heroku의 react에서 써야 함

--- src/app.js
+++ src/app.js
@@ -22,10 +22,6 @@ app.use(express.static(path.join(__dirname, "front", "build")))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 기본 페이지
-app.get("/", (req, res) => {
-  res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
-});
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
